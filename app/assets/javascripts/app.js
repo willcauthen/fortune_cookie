39f40b73e34a1f2718763a6dd0eabafa68911d74
Will Cauthen <willcauthen50@gmail.com>
2015-12-13 16:07:23 -0800

    image updates and fortune is revealed when cookie clicked

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,19 @@
 console.log("We're in good shape");
+var prophesy;
 
+function preparePage() {
+	prophesy = document.getElementById("prophesy");
+	console.log(prophesy);
+	prophesy.style.display = "none";
+
+}
 function smash() {
 	var cookie = document.getElementById("cookie");
 	// cookie.style.display = "none";
 	if (cookie.src == "http://f.tqn.com/y/webclipart/1/S/z/I/6/Fortune-Cookie.png") {
-		console.log("img src was a match");
-		cookie.src = "https://cdn0.iconfinder.com/data/icons/common-warnings/512/explosion_crack_rock-512.png";
-		cookie.style.background = "white";
-	}
-	else {
-		cookie.src = "http://f.tqn.com/y/webclipart/1/S/z/I/6/Fortune-Cookie.png";
-	}
+		cookie.src = "http://coolkidfacts.com/wp-content/uploads/2014/07/explosion.png";
 	console.log("it was clicked");
-// https://cdn0.iconfinder.com/data/icons/common-warnings/512/explosion_crack_rock-512.png
+		prophesy.style.display = "block";
+	} 
 	
 }
