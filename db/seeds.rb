89837c533068b2cf318be767b7ea335d59a17430
Will Cauthen <willcauthen50@gmail.com>
2015-12-23 12:18:36 -0500

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# User.create(user_name: "wilbo", email: "wtf@bbq.com", password_digest: "password", score: 0)
-
 Fortune.create({ quote: "you would rather die than face the truth",
  author: "unknown"})
 
